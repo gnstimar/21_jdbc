66acd9fe88858443c7af8c6daf0ecc1e1754520b
Agnes Timar <timaragnes@outlook.hu>
2026-08-18 20:07:42 +0200

    The simplest connection and query

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,17 +1,52 @@
 package se.lexicon;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/school_management";
+    private static final String USER = "root";
+    private static final String PASSWORD = "12345678";
+
+    static void main() {
+        /*
+         * STEPS
+         * Load JDBC Driver
+         * Establish Database Connection
+         * Create Statement / PreparedStatement
+         * Execute SQL Query
+         * Process ResultSet
+         * Close JDBC Resources
+         * */
+
+
+        // Establish Database Connection + Closing happens here too
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             // Create Statement
+             Statement statement = connection.createStatement()
+        ) {
+            IO.println("Database connection successfully established.");
+
+            String query = "SELECT id, name, class_group, create_date FROM student;";
+
+            // Execute Query
+            ResultSet resultSet = statement.executeQuery(query);
+            IO.println("Any result?" + resultSet);
+            // Process ResultSet
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String class_group = resultSet.getString("class_group");
+                LocalDateTime createDate = resultSet.getTimestamp("create_date").toLocalDateTime();
+                String formattedDateTime = createDate.format(DateTimeFormatter.ofPattern("EEEE MMMM dd yyyy"));
+
+                IO.println("ID: " + id + "| name: " + name + "| class_group: " + class_group + "| create_date: " + formattedDateTime);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
